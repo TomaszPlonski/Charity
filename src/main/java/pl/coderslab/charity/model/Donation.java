1e221b7c35c5aee6ceca67e7d4f5dcc1686aac2f
TomaszPlonski <tom.plonski@gmail.com>
2022-06-23 19:47:29 +0200

    Donation entity and repository

---
+++ src/main/java/pl/coderslab/charity/model/Donation.java
@@ -0,0 +1,49 @@
+package pl.coderslab.charity.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="donations")
+public class Donation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    Integer quantity;
+
+    @ManyToMany
+    @JoinTable(
+            name = "donations_categories",
+            joinColumns = @JoinColumn(name="donation_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
+    List<Category> categories;
+
+    @ManyToOne @JoinColumn(name="institution_id")
+    Institution institution;
+
+    String street;
+
+    String city;
+
+    String zipCode;
+
+    LocalDate pickUpDate;
+
+    LocalTime pickUpTime;
+
+    String pickUpComment;
+}
