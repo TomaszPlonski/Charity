c4eca1da40724545ab4a5b7fa05beb79785449be
TomaszPlonski <tom.plonski@gmail.com>
2022-06-23 20:48:27 +0200

    added bags sum QUERY

--- src/main/java/pl/coderslab/charity/repository/DonationRepository.java
+++ src/main/java/pl/coderslab/charity/repository/DonationRepository.java
@@ -1,7 +1,13 @@
 package pl.coderslab.charity.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pl.coderslab.charity.model.Donation;
 
 public interface DonationRepository extends JpaRepository<Donation,Long> {
+
+    @Query("SELECT SUM(quantity) FROM Donation")
+    Long selectTotalsQuantity();
+
+
 }
