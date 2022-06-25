2be1135b1b3ff97962786a15bbe428144afaea07
TomaszPlonski <tom.plonski@gmail.com>
2022-06-25 17:15:55 +0200

    CategoryDto

---
+++ src/main/java/pl/coderslab/charity/dto/CategoryDto.java
@@ -0,0 +1,17 @@
+package pl.coderslab.charity.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryDto {
+
+    Long id;
+
+    String name;
+}
