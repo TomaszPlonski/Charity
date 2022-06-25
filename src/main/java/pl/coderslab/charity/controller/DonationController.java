b9cb0566552910c5be0f5ee1fd023090f67285fc
TomaszPlonski <tom.plonski@gmail.com>
2022-06-25 17:17:21 +0200

    DonationController

---
+++ src/main/java/pl/coderslab/charity/controller/DonationController.java
@@ -0,0 +1,82 @@
+package pl.coderslab.charity.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import pl.coderslab.charity.dto.CategoryDto;
+import pl.coderslab.charity.dto.FormDto;
+import pl.coderslab.charity.service.form.FormService;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@SessionAttributes("form")
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/form")
+public class DonationController {
+
+    private final FormService formService;
+
+    @GetMapping
+    public String formGetAction(Model model){
+        model.addAttribute("form",formService.createFormDto());
+        return "form/form1";
+    }
+
+    @PostMapping(params = "form1Submit")
+    public String form1Submit(@Valid @ModelAttribute("form") FormDto form,
+                              BindingResult bindingResult){
+        return "form/form2";
+    }
+
+    @PostMapping(params = "form2Submit")
+    public String form2Submit(@Valid @ModelAttribute("form") FormDto form,
+                              BindingResult bindingResult){
+        return "form/form3";
+    }
+    @PostMapping(params = "form2Back")
+    public String form2Back(@Valid @ModelAttribute("form") FormDto form,
+                            BindingResult bindingResult){
+        return "form/form1";
+    }
+
+    @PostMapping(params = "form3Submit")
+    public String form3Submit(@Valid @ModelAttribute("form") FormDto form,
+                              BindingResult bindingResult){
+        return "form/form4";
+    }
+    @PostMapping(params = "form3Back")
+    public String form3Back(@Valid @ModelAttribute("form") FormDto form,
+                            BindingResult bindingResult){
+        return "form/form2";
+    }
+
+    @PostMapping(params = "form4Submit")
+    public String form4Submit(@Valid @ModelAttribute("form") FormDto form,
+                              BindingResult bindingResult){
+        return "form/form5";
+    }
+    @PostMapping(params = "form4Back")
+    public String form4Back(@Valid @ModelAttribute("form") FormDto form,
+                            BindingResult bindingResult){
+        return "form/form3";
+    }
+
+    @PostMapping(params = "form5Submit")
+    public String form5Submit(@Valid @ModelAttribute("form") FormDto form,
+                              BindingResult bindingResult){
+        return "form/form-confirmation";
+    }
+    @PostMapping(params = "form5Back")
+    public String form5Back(@Valid @ModelAttribute("form") FormDto form,
+                            BindingResult bindingResult){
+        return "form/form4";
+    }
+
+
+}
--- src/main/java/pl/coderslab/charity/controller/HomeController.java
+++ src/main/java/pl/coderslab/charity/controller/HomeController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.coderslab.charity.service.home.HomeService;
 import pl.coderslab.charity.service.home.HomeServiceImpl;
@@ -16,15 +17,10 @@ public class HomeController {
     private final HomeService homeService;
 
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String homeAction(Model model){
         model.addAttribute( "homeInfo",homeService.getInfoForHomePage());
         return "index";
-    }
-
-    @RequestMapping("/form")
-    public String formAction(Model model){
-        return "/form";
     }
 
 }
