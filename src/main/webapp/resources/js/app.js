9fd7274b8521eef5cb4aea1230f607dac7da1549
TomaszPlonski <tom.plonski@gmail.com>
2022-06-25 17:15:15 +0200

    app.js form stages off

--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -91,84 +91,84 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   });
 
-  /**
-   * Switching between form steps
-   */
-  class FormSteps {
-    constructor(form) {
-      this.$form = form;
-      this.$next = form.querySelectorAll(".next-step");
-      this.$prev = form.querySelectorAll(".prev-step");
-      this.$step = form.querySelector(".form--steps-counter span");
-      this.currentStep = 1;
-
-      this.$stepInstructions = form.querySelectorAll(".form--steps-instructions p");
-      const $stepForms = form.querySelectorAll("form > div");
-      this.slides = [...this.$stepInstructions, ...$stepForms];
-
-      this.init();
-    }
-
-    /**
-     * Init all methods
-     */
-    init() {
-      this.events();
-      this.updateForm();
-    }
-
-    /**
-     * All events that are happening in form
-     */
-    events() {
-      // Next step
-      this.$next.forEach(btn => {
-        btn.addEventListener("click", e => {
-          e.preventDefault();
-          this.currentStep++;
-          this.updateForm();
-        });
-      });
-
-      // Previous step
-      this.$prev.forEach(btn => {
-        btn.addEventListener("click", e => {
-          e.preventDefault();
-          this.currentStep--;
-          this.updateForm();
-        });
-      });
-
-      // Form submit
-      this.$form.querySelector("form").addEventListener("submit", e => this.submit(e));
-    }
-
-    /**
-     * Update form front-end
-     * Show next or previous section etc.
-     */
-    updateForm() {
-      this.$step.innerText = this.currentStep;
-
-      // TODO: Validation
-
-      this.slides.forEach(slide => {
-        slide.classList.remove("active");
-
-        if (slide.dataset.step == this.currentStep) {
-          slide.classList.add("active");
-        }
-      });
-
-      this.$stepInstructions[0].parentElement.parentElement.hidden = this.currentStep >= 5;
-      this.$step.parentElement.hidden = this.currentStep >= 5;
-
-      // TODO: get data from inputs and show them in summary
-    }
-
-  }
-  const form = document.querySelector(".form--steps");
-  if (form !== null) {
-    new FormSteps(form);
-  }
+  // /**
+  //  * Switching between form steps
+  //  */
+  // class FormSteps {
+  //   constructor(form) {
+  //     this.$form = form;
+  //     this.$next = form.querySelectorAll(".next-step");
+  //     this.$prev = form.querySelectorAll(".prev-step");
+  //     this.$step = form.querySelector(".form--steps-counter span");
+  //     this.currentStep = 1;
+  //
+  //     this.$stepInstructions = form.querySelectorAll(".form--steps-instructions p");
+  //     const $stepForms = form.querySelectorAll("form > div");
+  //     this.slides = [...this.$stepInstructions, ...$stepForms];
+  //
+  //     this.init();
+  //   }
+  //
+  //   /**
+  //    * Init all methods
+  //    */
+  //   init() {
+  //     this.events();
+  //     this.updateForm();
+  //   }
+  //
+  //   /**
+  //    * All events that are happening in form
+  //    */
+  //   events() {
+  //     // Next step
+  //     this.$next.forEach(btn => {
+  //       btn.addEventListener("click", e => {
+  //         e.preventDefault();
+  //         this.currentStep++;
+  //         this.updateForm();
+  //       });
+  //     });
+  //
+  //     // Previous step
+  //     this.$prev.forEach(btn => {
+  //       btn.addEventListener("click", e => {
+  //         e.preventDefault();
+  //         this.currentStep--;
+  //         this.updateForm();
+  //       });
+  //     });
+  //
+  //     // Form submit
+  //     this.$form.querySelector("form").addEventListener("submit", e => this.submit(e));
+  //   }
+  //
+  //   /**
+  //    * Update form front-end
+  //    * Show next or previous section etc.
+  //    */
+  //   updateForm() {
+  //     this.$step.innerText = this.currentStep;
+  //
+  //     // TODO: Validation
+  //
+  //     this.slides.forEach(slide => {
+  //       slide.classList.remove("active");
+  //
+  //       if (slide.dataset.step == this.currentStep) {
+  //         slide.classList.add("active");
+  //       }
+  //     });
+  //
+  //     this.$stepInstructions[0].parentElement.parentElement.hidden = this.currentStep >= 5;
+  //     this.$step.parentElement.hidden = this.currentStep >= 5;
+  //
+  //     // TODO: get data from inputs and show them in summary
+  //   }
+  //
+  // }
+  // const form = document.querySelector(".form--steps");
+  // if (form !== null) {
+  //   new FormSteps(form);
+  // }
 });
